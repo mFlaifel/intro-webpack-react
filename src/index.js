61cfc4c19ca65baae9491291ce593e622307bd70
mFlaifel <msf-2oo7@hotmail.com>
2020-05-05 03:08:03 +0300

    third step

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css'
+// import './style.css'
 const element = <p className="p">Hello g8</p>
 
 ReactDOM.render(element, document.getElementById('root'));
